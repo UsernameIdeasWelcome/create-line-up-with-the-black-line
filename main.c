1c8acab63a9dd5a9f393c88294161e6ffa19ca4d
Affinity Robotics <47459654+AffinityRobotics@users.noreply.github.com>
2019-02-14 19:31:54 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-//this code is lining the create up with the black line
-//Ryan Sizemore, 2/14
+//This code whas made by Jacob brown on 2/8/2019 
+//This code is lining the create up with the black line
 #include <kipr/botball.h>
 
-//I'm creating a function
+//I'm creating A function
 void find_the_line(int black,int curve,int speed);
 
 int main()
 {
-    //I'm creating my variables  
+    //I'm createing my vairiables  
     int black = 2700; 		
     int speed = 200;	
     int curve = 180;					
@@ -23,12 +23,12 @@ int main()
     return 0;
 
 }
-//I'm defining my function
+//I'm difining my function
 void find_the_line(int black,int curve,int speed)
 {
-    while ( get_create_rcliff_amt() >= black || get_create_lcliff_amt() >= black )//do the sensors see white
+    while ( get_create_rcliff_amt() >= black || get_create_lcliff_amt() >= black )//do the sensers see white
     {
-        if ( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black )//do both the sensors see white
+        if ( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black )//do both the sensers see white
         {
             create_drive(speed, curve);
             msleep(50);
@@ -36,13 +36,13 @@ void find_the_line(int black,int curve,int speed)
             create_drive(speed, -curve);
             msleep(50);
         }
-        else if ( black >= get_create_rcliff_amt() )//does the right sensors see black
+        else if ( black >= get_create_rcliff_amt() )//does the right sensers see black
         {
             create_drive(100, -50);//The create is turning left
 
         }
 
-        else if ( black >= get_create_lcliff_amt() )//does the left sensors see black
+        else if ( black >= get_create_lcliff_amt() )//does the left sensers see black
         {
             create_drive(100, 50);//The create is turning right
 
@@ -50,7 +50,7 @@ void find_the_line(int black,int curve,int speed)
 
         else if( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black)//do both the sensers see black
         {
-            break; //this breaks the while loop            
+            break; //This breaks the while loop            
 
         }
     }
