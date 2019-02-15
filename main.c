6c76f0c9a3f794305b6898956b561aa4115691c4
Affinity Robotics <47459654+AffinityRobotics@users.noreply.github.com>
2019-02-14 19:28:32 -0600

    Added name and spelling errors

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 //this code is lining the create up with the black line
+//Ryan Sizemore, 2/14
 #include <kipr/botball.h>
 
 //I'm creating A function
@@ -6,7 +7,7 @@ void find_the_line(int black,int curve,int speed);
 
 int main()
 {
-    //I'm createing my vairiables  
+    //I'm creating my variables  
     int black = 2700; 		
     int speed = 200;	
     int curve = 180;					
@@ -22,12 +23,12 @@ int main()
     return 0;
 
 }
-//I'm difining my function
+//I'm defining my function
 void find_the_line(int black,int curve,int speed)
 {
-    while ( get_create_rcliff_amt() >= black || get_create_lcliff_amt() >= black )//do the sensers see white
+    while ( get_create_rcliff_amt() >= black || get_create_lcliff_amt() >= black )//do the sensors see white
     {
-        if ( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black )//do both the sensers see white
+        if ( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black )//do both the sensors see white
         {
             create_drive(speed, curve);
             msleep(50);
@@ -35,13 +36,13 @@ void find_the_line(int black,int curve,int speed)
             create_drive(speed, -curve);
             msleep(50);
         }
-        else if ( black >= get_create_rcliff_amt() )//does the right sensers see black
+        else if ( black >= get_create_rcliff_amt() )//does the right sensors see black
         {
             create_drive(100, -50);//The create is turning left
 
         }
 
-        else if ( black >= get_create_lcliff_amt() )//does the left sensers see black
+        else if ( black >= get_create_lcliff_amt() )//does the left sensors see black
         {
             create_drive(100, 50);//The create is turning right
 
