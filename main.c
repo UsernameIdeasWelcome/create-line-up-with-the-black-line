1491b0a78522a71d50a8f7a6c50943a470e5b12d
Affinity Robotics <47459654+AffinityRobotics@users.noreply.github.com>
2019-02-14 19:32:43 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
-//This code whas made by Jacob brown on 2/8/2019 
-//This code is lining the create up with the black line
+//This code is lining the create up with the black line.
+//John Stearman 2/14/2019.
 #include <kipr/botball.h>
 
-//I'm creating A function
+//I'm creating A function.
 void find_the_line(int black,int curve,int speed);
 
 int main()
 {
-    //I'm createing my vairiables  
+    //I'm creating my vairiables.
     int black = 2700; 		
     int speed = 200;	
     int curve = 180;					
 
-    create_connect();//I'm connecting to the create
+    create_connect();//I'm connecting to the create.
 
-    find_the_line(black,curve,speed);//I'm calling my function
+    find_the_line(black,curve,speed);//I'm calling my function.
 
 
 
-    create_disconnect();//I'm disconnecting to the create
+    create_disconnect();//I'm disconnecting to the create.
 
     return 0;
 
 }
-//I'm difining my function
+//I'm defining my function.
 void find_the_line(int black,int curve,int speed)
 {
-    while ( get_create_rcliff_amt() >= black || get_create_lcliff_amt() >= black )//do the sensers see white
+    while ( get_create_rcliff_amt() >= black || get_create_lcliff_amt() >= black )//Do the sensers see white?
     {
-        if ( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black )//do both the sensers see white
+        if ( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black )//Do both the sensers see white?
         {
             create_drive(speed, curve);
             msleep(50);
@@ -36,21 +36,21 @@ void find_the_line(int black,int curve,int speed)
             create_drive(speed, -curve);
             msleep(50);
         }
-        else if ( black >= get_create_rcliff_amt() )//does the right sensers see black
+        else if ( black >= get_create_rcliff_amt() )//Does the right sensers see black?
         {
-            create_drive(100, -50);//The create is turning left
+            create_drive(100, -50);//The create is turning left.
 
         }
 
-        else if ( black >= get_create_lcliff_amt() )//does the left sensers see black
+        else if ( black >= get_create_lcliff_amt() )//Does the left sensers see black?
         {
-            create_drive(100, 50);//The create is turning right
+            create_drive(100, 50);//The create is turning right.
 
         }
 
-        else if( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black)//do both the sensers see black
+        else if( get_create_rcliff_amt() >= black && get_create_lcliff_amt() >= black)//Do both the sensers see black?
         {
-            break; //This breaks the while loop            
+            break; //This breaks the while loop.       
 
         }
     }
